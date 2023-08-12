ab871948adbe629a310faf5ac0f95bc9973d14c0
MartinNeifert <martybn@gmail.com>
2023-08-12 13:50:18 -0700

    Checkpount

--- art/swissgl/sketch.js
+++ art/swissgl/sketch.js
@@ -2,21 +2,33 @@
 const canvas = document.getElementById('sketch3');
 const glsl = SwissGL(canvas);
 
-const numberColors = 10;
 const worldExtent = 70.0;
-const seed = 123;
+const forceFactor = 0.3;
 
 const settings = {
     repulsion: 20,
     inertia: 1,
-    dt: 0.04,
+    forceFactor: 0.2,
+
+    // refresh settings
+    seed: 1,
+    numberColors: 4,
+    numberParticles: 1000,
 }
 
 const gui = new dat.GUI();
 
 gui.add(settings, 'repulsion', .1, 100).step(.1);
 gui.add(settings, 'inertia', .1, 10).step(.01);
-gui.add(settings, 'dt', .001, 1).step(.001);
+gui.add(settings, 'forceFactor', .001, 1).step(.001);
+
+const refreshSettings = gui.addFolder('refresh settings')
+refreshSettings.add(settings, 'seed', 1, 100).step(1);
+refreshSettings.add(settings, 'numberColors', 1, 100).step(1);
+refreshSettings.add(settings, 'numberParticles', 10, 10000).step(10);
+refreshSettings.add({refresh: function() {
+    init();
+}}, 'refresh');
 
 let showDatGui = document.getElementsByName("showDatGui")[0];
 if(showDatGui && showDatGui.content == "false") {
@@ -25,25 +37,37 @@ if(showDatGui && showDatGui.content == "false") {
     gui.show();
 }
 
-const F = glsl(
-    {
-        numberColors, 
-        FP:`float(I.x==I.y) + 0.3*float(I.x==(I.y+1)%int(numberColors))`
-    },
-    {size:[numberColors,numberColors], format:'r16f', tag:'F'});
+var F;
 
-const points = glsl({}, {size:[50,100], story:100,
-                        format:'rgba32f', tag:'points'});
+var points;
 
-glsl({numberColors, seed, FP:`
-    vec2 pos = (hash(ivec3(I, seed)).xy-0.5)*10.0;
-    float color = floor(UV.x*numberColors);
-    FOut = vec4(pos, 0.0, color);`},
-    points);
+
+
+function init() {
+    F = glsl(
+        {
+            numberColors: settings.numberColors, forceFactor, seed: settings.seed,
+            FP:`
+                hash(ivec3(I, seed)).x - 0.5
+            `
+        },
+        {size:[settings.numberColors,settings.numberColors], format:'r16f', tag:'F'});
+
+        
+    points = glsl({}, {size:[100,200], story:2000,
+                format:'rgba32f', tag:'points'});
+    
+
+    glsl({numberColors: settings.numberColors, seed: settings.seed, FP:`
+        vec2 pos = (hash(ivec3(I, seed)).xy-0.5)*20.0;
+        float color = floor(UV.x*numberColors);
+        FOut = vec4(pos, 0.0, color);`},
+        points);
+}
 
 function step() {
     for (let i=0; i<1; ++i)
-    glsl({F, worldExtent, repulsion: settings.repulsion/10, inertia: settings.inertia / 10, dt: settings.dt, past: points[1], 
+    glsl({F, worldExtent, repulsion: settings.repulsion/10, inertia: settings.inertia / 10, dt: settings.forceFactor, past: points[1], 
     FP:`
     vec3 wrap(vec3 p) {
         return (fract(p/worldExtent+0.5)-0.5)*worldExtent;
@@ -63,23 +87,24 @@ function step() {
             float att = f*max(1.0-abs(r-2.0), 0.0);
             force += dpos*(att-rep);
         }
-        vec3 vel = wrap(FOut.xyz-past(I).xyz)*pow(inertia, dt);
-        FOut.xyz = wrap(FOut.xyz+vel+0.5*force*(dt*dt));
+        FOut.xyz = wrap(FOut.xyz+0.5*force*(dt*dt));
+
     }
     `}, points);
 }
 
 function draw() {
     step();
-    glsl({numberColors, worldExtent, points: points[0], Grid: points[0].size,
+    glsl({numberColors: settings.numberColors, worldExtent, points: points[0], Grid: points[0].size,
             Aspect:'cover', Blend: 'd*(1-sa)+s*sa', Inc:`
         varying vec3 color;`, 
         VP:`
         vec4 d = points(ID.xy);
         color = cos((d.w/numberColors+vec3(0,0.33,0.66))*TAU)*0.5+0.5;
         VPos.xy = 2.0*(d.xy+XY/8.0)/worldExtent;`, 
-        FP:`color, smoothstep(1.0, 0.5, length(XY))`});
+        FP:`color, smoothstep(1.0, 0.1, length(XY))`});
     requestAnimationFrame(draw);
 }
+init();
 requestAnimationFrame(draw);
 })();
